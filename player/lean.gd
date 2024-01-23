45d17b59773aa979c9a7dd1da80297288b32e08f
David <37672966+Ieida@users.noreply.github.com>
2024-01-23 22:55:38 +0100

    Lean improvements

--- player/lean.gd
+++ player/lean.gd
@@ -4,24 +4,19 @@ enum {LEFT, RIGHT, NONE}
 
 var side = NONE
 @export var angle: float = 20
-@export var distance: float = 0.1
 
 func _process(delta):
 	if Input.is_action_just_pressed("lean_right"):
 		if side == RIGHT:
 			rotation_degrees.z = 0
-			position.x = 0
 			side = NONE
 		else:
 			rotation_degrees.z = -angle
-			position.x = distance
 			side = RIGHT
 	if Input.is_action_just_pressed("lean_left"):
 		if side == LEFT:
 			rotation_degrees.z = 0
-			position.x = 0
 			side = NONE
 		else:
 			rotation_degrees.z = angle
-			position.x = -distance
 			side = LEFT
