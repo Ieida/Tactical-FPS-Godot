f4e1377dd65e89e69cb694f19f5e007f3464ba39
David <37672966+Ieida@users.noreply.github.com>
2024-02-16 10:36:13 +0100

    leaning improvements

--- player/lean.gd
+++ player/lean.gd
@@ -3,20 +3,25 @@ extends Node3D
 enum {LEFT, RIGHT, NONE}
 
 var side = NONE
-@export var angle: float = 20
+@export var angle: float = 10
+@onready var camera: Node3D = $PlayerCamera
 
 func _process(delta):
 	if Input.is_action_just_pressed("lean_right"):
 		if side == RIGHT:
 			rotation_degrees.z = 0
+			camera.position.x = 0
 			side = NONE
 		else:
 			rotation_degrees.z = -angle
+			camera.position.x = 0.2
 			side = RIGHT
 	if Input.is_action_just_pressed("lean_left"):
 		if side == LEFT:
 			rotation_degrees.z = 0
+			camera.position.x = 0
 			side = NONE
 		else:
 			rotation_degrees.z = angle
+			camera.position.x = -0.2
 			side = LEFT
