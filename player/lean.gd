41fc95f00433a84250f889eecfda257f840ac346
David <37672966+Ieida@users.noreply.github.com>
2024-02-17 13:06:57 +0100

    improved player leaning

--- player/lean.gd
+++ player/lean.gd
@@ -3,25 +3,35 @@ extends Node3D
 enum {LEFT, RIGHT, NONE}
 
 var side = NONE
-@export var angle: float = 10
+@export var angle: float = 5
+@export var distance: float = 0.3
 @onready var camera: Node3D = $PlayerCamera
+var tween: Tween
 
 func _process(delta):
 	if Input.is_action_just_pressed("lean_right"):
 		if side == RIGHT:
-			rotation_degrees.z = 0
-			camera.position.x = 0
+			if tween: tween.stop()
+			tween = create_tween().set_parallel(true)
+			tween.tween_property(self, "rotation_degrees:z", 0, 0.5)
+			tween.tween_property(camera, "position:x", 0, 0.5)
 			side = NONE
 		else:
-			rotation_degrees.z = -angle
-			camera.position.x = 0.2
+			if tween: tween.stop()
+			tween = create_tween().set_parallel(true)
+			tween.tween_property(self, "rotation_degrees:z", -angle, 0.5)
+			tween.tween_property(camera, "position:x", distance, 0.5)
 			side = RIGHT
 	if Input.is_action_just_pressed("lean_left"):
 		if side == LEFT:
-			rotation_degrees.z = 0
-			camera.position.x = 0
+			if tween: tween.stop()
+			tween = create_tween().set_parallel(true)
+			tween.tween_property(self, "rotation_degrees:z", 0, 0.5)
+			tween.tween_property(camera, "position:x", 0, 0.5)
 			side = NONE
 		else:
-			rotation_degrees.z = angle
-			camera.position.x = -0.2
+			if tween: tween.stop()
+			tween = create_tween().set_parallel(true)
+			tween.tween_property(self, "rotation_degrees:z", angle, 0.5)
+			tween.tween_property(camera, "position:x", -distance, 0.5)
 			side = LEFT
